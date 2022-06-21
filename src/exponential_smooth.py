c7fcb85d65681e887130685befbb35385fe4e0b4
Nataliia Solodovnikova <74897482+notasha@users.noreply.github.com>
2022-06-21 17:39:15 +0300

    Update exponential_smooth.py

--- src/exponential_smooth.py
+++ src/exponential_smooth.py
@@ -22,17 +22,18 @@ def get_optimal_coefficient(traj_sigma2, measure_sigma2):
     return (-Xi + np.sqrt(abs(Xi**2 + 4 * Xi))) / 2
 
 def exponential_smoothing(initial_state, measurements, smooth_coefficient):
-    points_num = measurements.shape[0]
-    x_smooth = np.empty(points_num)
+    N = measurements.shape[0]
+    x_smooth = np.empty(measurements.shape)
     x_smooth[0] = initial_state
-    for i in range(points_num):
+    for i in range(1, N):
         x_smooth[i] = x_smooth[i-1] + smooth_coefficient * (measurements[i] - x_smooth[i-1])
     return x_smooth
 
 def backward_exponential_smoothing(forward_exp_smooth, smooth_coefficient):
     N = forward_exp_smooth.shape[0]
-    bx_smooth = np.empty(N)
+    bx_smooth = np.empty(forward_exp_smooth.shape)
+
     bx_smooth[-1] = forward_exp_smooth[-1]  # define the last element
-    for i in range(N-2, 0, -1):
+    for i in range(N-2, -1, -1):
         bx_smooth[i] = bx_smooth[i+1] + smooth_coefficient * (forward_exp_smooth[i] - bx_smooth[i+1])
     return bx_smooth
